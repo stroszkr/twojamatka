f71e1fdd0a39c87a79d971aaf260c7fdb4e1e682
stroszkr <olektrebaczmatinf@gmail.com>
2024-03-04 14:46:34 +0100

    urmom

---
+++ script.js
@@ -0,0 +1,20 @@
+var waga;
+var wzrots;
+var bmi;
+
+wzrots = prompt("podae wzrots")
+readFloat(prompt)
+waga = prompt("podae wagae")
+readFloat(prompt)
+
+
+
+const urMom = "<3";
+console.log("Haiii!! ", urMom);
+
+function calcAge(birthYear)
+{
+    return 2025 - birthYear
+}
+by = calcAge(2008);
+console.log(by);
